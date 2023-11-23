d78dcc14067c7f71bd29decb8276b77b684ab0b4
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-11-23 16:23:27 -0400

    función parra buscar componente BuscaAPI

--- src/InputLoader.js
+++ src/InputLoader.js
@@ -8,4 +8,16 @@ export function Busqueda() {
     const [loading, setLoading] = useState(false);
     const [userResults, setUserResults] = useState([]); // Nuevo estado para los resultados de la búsqueda
     
+    
+
+    // Creamos la función que mandará al componente de la api de github
+    async function handleChange() {
+        const userInput = await inputValue.trim();
+        if (userInput) {
+          const results = await BuscarAPI(userInput);
+          setUserResults(results); // Guarda los resultados de la búsqueda en el estado
+        } else {
+          setUserResults([]);
+        }
+      }
 }  
