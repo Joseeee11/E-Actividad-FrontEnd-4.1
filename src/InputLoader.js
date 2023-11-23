86de70bb3e56faf5ddb15f4098e0dd2a265b55e0
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-11-23 16:21:03 -0400

    estado de react

--- src/InputLoader.js
+++ src/InputLoader.js
@@ -1,3 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import { BuscarAPI } from "./BuscarAPI";
 import { RenderUsers } from "./RenderUsers";
+
+export function Busqueda() {
+    // sacamos variables del react para saber el estado
+    const [inputValue, setInputValue] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [userResults, setUserResults] = useState([]); // Nuevo estado para los resultados de la búsqueda
+    
+}  
